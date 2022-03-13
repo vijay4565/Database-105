3dc3581a3f44be6bded5f48353213124a6811d41
vijay4565 <63921086+vijay4565@users.noreply.github.com>
2022-03-13 23:07:30 +0530

    Add files via upload

---
+++ 02-create-users-tables.sql
@@ -0,0 +1,8 @@
+CREATE TYPE employment_status AS ENUM('employed', 'self-employed','unemployed');
+
+CREATE TABLE users(
+ full_name VARCHAR(200),
+ yearly_salary INT,
+ current_status employment_status
+--current_status ENUM('employed', 'self-employed','unemployed')
+);
